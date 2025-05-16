29f029228a88e8ca623526769a28d6bbce51229f
b.pegoraro <bernardopegoraro2021@gmail.com>
2025-05-16 18:20:23 -0300

    suor

--- src/models/index.js
+++ src/models/index.js
@@ -1,19 +1,19 @@
-// import User from './UsersModel.js';
-// import Adress from "./AdressModel.js";
-// import Coupon from "./CouponsModel.js";
-// import Payment from "./PaymentsModel.js";
-// import Product from "./ProductsModel.js";
-// import Order from "./OrdersModel.js";
-// import OrderProduct from "./Orders_ProductsModel.js";
-// import Category from "./CategoriesModel.js";
+//  import User from './UsersModel.js';
+//  import Adress from "./AdressModel.js";
+//  import Coupon from "./CouponsModel.js";
+//  import Payment from "./PaymentsModel.js";
+//  import Product from "./ProductsModel.js";
+//  import Order from "./OrdersModel.js";
+//  import OrderProduct from "./Orders_ProductsModel.js";
+//  import Category from "./CategoriesModel.js";
 
 (async () => {
   // await User.sync({ force:true });
   // await Adress.sync({ force:true });
-  // await Coupon.sync({ force:true });
+  // await Coupon.sync({ force:tru-e });
   // await Category.sync({ force:true });
   // await Product.sync({ force:true });
-  // await Payment.sync({ force:true });
+  // await Payment.sync({ force:true }); 
   // await Order.sync({ force:true });
   // await OrderProduct.sync({ force:true });
-})();
+})(); 
